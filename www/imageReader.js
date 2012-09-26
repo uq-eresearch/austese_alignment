4ea844c08dad69dd7f9a9a56cb3dfea0a27631b8
Simon McNaughton <simon.mcnaughton@uq.edu.au>
2012-09-26 15:39:51 +1000

    Initial Commit 2

---
+++ www/imageReader.js
@@ -0,0 +1,158 @@
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+	});
+	return vars;
+}
+
+window.onload = function() {
+	br = new BookReader();
+
+	br.getPageWidth = function(index) {
+		return 800;
+	}
+
+	br.getPageHeight = function(index) {
+		return 1200;
+	}
+
+	br.getPageURI = function(index, reduce, rotate) {
+		var leafStr = '000';
+		var imgStr = (index+1).toString();
+		var re = new RegExp("0{"+imgStr.length+"}$");
+		if(getUrlVars()["url"]) {
+			var url = decodeURIComponent(getUrlVars()["url"]);
+		} else {
+			var url = 'http://www.archive.org/download/BookReader/img/page'+leafStr.replace(re, imgStr) + '.jpg';
+		}
+		return url;
+	}
+
+	br.getPageSide = function(index) {
+		if (0 == (index & 0x1)) {
+			return 'R';
+		} else {
+			return 'L';
+		}
+	}
+
+	br.getSpreadIndices = function(pindex) {   
+		var spreadIndices = [null, null]; 
+		if ('rl' == this.pageProgression) {
+			if (this.getPageSide(pindex) == 'R') {
+				spreadIndices[1] = pindex;
+				spreadIndices[0] = pindex + 1;
+			} else {
+				spreadIndices[0] = pindex;
+				spreadIndices[1] = pindex - 1;
+			}
+		} else {
+			if (this.getPageSide(pindex) == 'L') {
+				spreadIndices[0] = pindex;
+				spreadIndices[1] = pindex + 1;
+			} else {
+				spreadIndices[1] = pindex;
+				spreadIndices[0] = pindex - 1;
+			}
+		}
+		
+		return spreadIndices;
+	}
+
+	br.getPageNum = function(index) {
+		return index+1;
+	}
+
+	br.numLeafs = 1;
+
+	br.bookTitle= 'Open Library BookReader Presentation';
+	br.bookUrl  = 'http://openlibrary.org';
+
+	br.imagesBaseURL = '../BookReader/images/';
+
+	br.getEmbedCode = function(frameWidth, frameHeight, viewParams) {
+		return "Embed code not supported in bookreader demo.";
+	}
+
+	br.init();
+	
+	$('#BRtoolbar').hide();
+	$('#BRnav').hide();
+	$('#textSrch').hide();
+	$('#btnSrch').hide();
+
+	$('#offsetX').val(Math.round($('#pageID').offset().left));
+
+	if (getUrlVars()["editable"] == 'true') {
+		var areaSelect = $('#pageID').imgAreaSelect({
+			handles: true,
+			instance: true,
+			onSelectEnd: function(img, selection) {
+				$('#offsetX').val(Math.round($(img).offset().left));
+				$('#imageX1').val(selection.x1);
+				$('#imageY1').val(selection.y1);
+				$('#imageX2').val(selection.x2);
+				$('#imageY2').val(selection.y2);
+				$('#imageWidth').val(selection.width);
+			  $('#imageHeight').val(selection.height);
+			}
+		});  
+	} else {
+		$('#pageID').click(function() {
+			$("[selected=selected]").attr("selected","")
+				.css("background-color","rgb(127,127,0)")
+				.css("border","3px solid yellow");
+			parent.window.clearObjectUrl();
+			parent.window.clearSelectedText();
+		});
+	}
+}
+
+function resetImage(x1,y1,x2,y2) {
+	areaSelect.setOptions({ show: true });
+	areaSelect.setSelection(x1,y1,x2,y2);
+	areaSelect.update();
+}
+
+function selectionVisible() {
+	return !(areaSelect.getSelection().width == 0 && areaSelect.getSelection().height == 0);
+}
+
+function focusImageSelection(img) {
+	if (img.getAttribute('selected') != 'selected') {
+		$("[selected=selected]").attr("selected","")
+	    .css("background-color","rgb(127,127,0)")
+	    .css("border","3px solid yellow");
+		img.style.border = '3px dotted purple';
+		img.style.backgroundColor = 'rgb(127,0,127)';
+		img.setAttribute('selected','selected');
+		parent.window.setObjectUrl(img.getAttribute("objectUrl"));
+		parent.window.setSelectedText(img.getAttribute("objectUrl"));
+	}
+}
+
+function setSelectedImage(objectUrl) {
+	$("[selected=selected]").attr("selected","")
+		.css("background-color","rgb(127,127,0)")
+		.css("border","3px solid yellow");
+	
+	var img = $("[objectUrl='" + objectUrl + "']")[0];
+	img.style.border = '3px dotted purple';
+	img.style.backgroundColor = 'rgb(127,0,127)';
+	img.setAttribute('selected','selected');
+}
+
+function clearSelectedImage() {
+	$("[selected=selected]").attr("selected","")
+		.css("background-color","rgb(127,127,0)")
+		.css("border","3px solid yellow");
+}
+
+function getSelectedObjectUrl() {
+	var selectedDivs = $("[selected=selected]");
+	if (selectedDivs.length != 1) {
+		return;
+	}
+	return selectedDivs[0].getAttribute('objectUrl');
+}
---
+++ www/textReader.js
@@ -0,0 +1,91 @@
+function getUrlVars() {
+	var vars = {};
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+			vars[key] = value;
+	});
+	return vars;
+}
+
+window.onload = function() {
+	var url;
+	if(getUrlVars()["url"]) {
+			url = decodeURIComponent(getUrlVars()["url"]);
+	} else {
+			url = '/textinput.html';
+	}
+
+	$('#injected-text').load(url, function() {
+			setTimeout(iResize, 50);
+	});
+
+	$("#container-div-2").scroll(function () { 
+			$("#container-div-1").scrollTop($("#container-div-2").scrollTop());
+	});
+}
+
+function iResize() {
+	var sizeInt = document.getElementById('injected-text').offsetHeight;
+
+	document.getElementById('annotations-div').height = sizeInt + 'px';
+	document.getElementById('annotations-div').style.height = sizeInt + 'px';
+}
+
+function focusText(img) {
+	var startOffset = img.getAttribute('startOffset');
+	var endOffset = img.getAttribute('endOffset');
+	var injectedText = document.getElementById('injected-text');
+
+	if (window.getSelection && document.createRange) {
+		var sel = window.getSelection();
+		var range = document.createRange();
+		range.selectNodeContents(injectedText);
+		range.setStart(injectedText.childNodes[0], startOffset);
+		range.setEnd(injectedText.childNodes[0], endOffset);
+		sel.removeAllRanges();
+		sel.addRange(range);
+	} else if (document.selection && document.body.createTextRange) {
+		var textRange = document.body.createTextRange();
+		textRange.moveToElementText(injectedText);
+		textRange.setStart(injectedText.childNodes[0], startOffset);
+		textRange.setEnd(injectedText.childNodes[0], endOffset);
+		textRange.select();
+	}
+}
+
+function highlightImage(img) {
+  if (img.getAttribute('selected') != 'selected') {
+	  $("[selected=selected]").attr("selected","")
+	    .attr("src","http://localhost/link_black.png");
+    img.src = 'http://localhost/link_yellow.png';
+    img.setAttribute('selected','selected');
+    parent.window.setObjectUrl(img.getAttribute("objectUrl"));
+    parent.window.setSelectedImage(img.getAttribute("objectUrl"));
+
+    focusText(img);
+  }
+}
+
+function setSelectedText(objectUrl) {
+  $("[selected=selected]").attr("selected","")
+	    .attr("src","http://localhost/link_black.png");
+  var img = $("[objectUrl=" + objectUrl + "]")[0];
+  img.setAttribute("selected","selected");
+  img.setAttribute("src","http://localhost/link_yellow.png");
+
+  focusText(img);
+}
+
+function clearSelectedText() {
+  $("[selected=selected]").attr("selected","")
+	  .attr("src","http://localhost/link_black.png");
+
+  if (window.getSelection) {
+  	if (window.getSelection().empty) {
+    	window.getSelection().empty();
+  	} else if (window.getSelection().removeAllRanges) {
+      window.getSelection().removeAllRanges();
+  	}
+  } else if (document.selection) {
+  	document.selection.empty();
+  }
+}
