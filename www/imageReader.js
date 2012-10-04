51870974a4d92e8af7bb89274059dda19af71ddd
Anna Gerber <anna.m.gerber@gmail.com>
2012-10-04 15:05:20 +1000

    Make formatting consistent

--- www/imageReader.js
+++ www/imageReader.js
@@ -1,157 +1,148 @@
 function getUrlVars() {
-  var vars = {};
-  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-      vars[key] = value;
-  });
-  return vars;
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
 }
 
 var areaSelect;
 
 jQuery(document).ready(function() {
-  br = new BookReader();
-    jQuery.extend(br,
-        {
-	    getPageWidth : function(index) {
-		
-		return 800;
-	    },
+    br = new BookReader();
+    jQuery.extend(br, {
+        getPageWidth : function(index) {
 
-	    getPageHeight : function(index) {
-		return 1200;
-	    },
-	    getPageURI : function(index, reduce, rotate) {
-		if(getUrlVars()["url"]) {
-		    var url = decodeURIComponent(getUrlVars()["url"]);
-		} else {
-		    var url = '/example/SILTS163.jpg';
-		}
-		return url;
-	    },
-	    getPageSide : function(index) {
-		if (0 == (index & 0x1)) {
-		    return 'R';
-		} else {
-		    return 'L';
-		}
-	    },
-	    getSpreadIndices : function(pindex) {   
-	        var spreadIndices = [null, null]; 
-		if ('rl' == this.pageProgression) {
-		  if (this.getPageSide(pindex) == 'R') {
-		    spreadIndices[1] = pindex;
-		    spreadIndices[0] = pindex + 1;
-		  } else {
-		    spreadIndices[0] = pindex;
-		    spreadIndices[1] = pindex - 1;
-		  }
-		} else {
-		  if (this.getPageSide(pindex) == 'L') {
-		    spreadIndices[0] = pindex;
-		    spreadIndices[1] = pindex + 1;
-		  } else {
-		    spreadIndices[1] = pindex;
-		    spreadIndices[0] = pindex - 1;
-		  }
-		}
+            return 800;
+        },
 
-		return spreadIndices;
-	    },
-	    getPageNum : function(index) {
-	        return index+1;
-	    },
-	    numLeafs : 1,
-	    bookTitle : 'Image Viewer',
-	    bookUrl  : 'http://austese.net',
+        getPageHeight : function(index) {
+            return 1200;
+        },
+        getPageURI : function(index, reduce, rotate) {
+            if (getUrlVars()["url"]) {
+                var url = decodeURIComponent(getUrlVars()["url"]);
+            } else {
+                var url = '/example/SILTS163.jpg';
+            }
+            return url;
+        },
+        getPageSide : function(index) {
+            if (0 == (index & 0x1)) {
+                return 'R';
+            } else {
+                return 'L';
+            }
+        },
+        getSpreadIndices : function(pindex) {
+            var spreadIndices = [null, null];
+            if ('rl' == this.pageProgression) {
+                if (this.getPageSide(pindex) == 'R') {
+                    spreadIndices[1] = pindex;
+                    spreadIndices[0] = pindex + 1;
+                } else {
+                    spreadIndices[0] = pindex;
+                    spreadIndices[1] = pindex - 1;
+                }
+            } else {
+                if (this.getPageSide(pindex) == 'L') {
+                    spreadIndices[0] = pindex;
+                    spreadIndices[1] = pindex + 1;
+                } else {
+                    spreadIndices[1] = pindex;
+                    spreadIndices[0] = pindex - 1;
+                }
+            }
 
-	    imagesBaseURL : '../BookReader/images/',
+            return spreadIndices;
+        },
+        getPageNum : function(index) {
+            return index + 1;
+        },
+        numLeafs : 1,
+        bookTitle : 'Image Viewer',
+        bookUrl : 'http://austese.net',
 
-	    getEmbedCode : function(frameWidth, frameHeight, viewParams) {
-	        return "Embed code not supported";
-	     }
+        imagesBaseURL : '../BookReader/images/',
 
-       }
-   );
-
-  br.init();
-  
-  jQuery('#BRtoolbar').hide();
-  jQuery('#BRnav').hide();
-  jQuery('#textSrch').hide();
-  jQuery('#btnSrch').hide();
-
-  if (getUrlVars()["editable"] == 'true') {
-    areaSelect = jQuery('#pagediv0').children().imgAreaSelect({
-      handles: true,
-      instance: true,
-      onSelectEnd: function(img, selection) {
-        jQuery('#offsetX').val(Math.round(jQuery(img).offset().left));
-        jQuery('#imageX1').val(selection.x1);
-        jQuery('#imageY1').val(selection.y1);
-        jQuery('#imageX2').val(selection.x2);
-        jQuery('#imageY2').val(selection.y2);
-        jQuery('#imageWidth').val(selection.width);
-        jQuery('#imageHeight').val(selection.height);
-      }
-    });  
-  } else {
-    jQuery('#pagediv0').children().click(function() {
-      jQuery("[selected=selected]").attr("selected","")
-        .css("background-color","rgb(127,127,0)")
-        .css("border","3px solid yellow");
-      parent.window.clearObjectUrl();
-      parent.window.clearSelectedText();
+        getEmbedCode : function(frameWidth, frameHeight, viewParams) {
+            return "Embed code not supported";
+        }
     });
-  }
+
+    br.init();
+
+    jQuery('#BRtoolbar').hide();
+    jQuery('#BRnav').hide();
+    jQuery('#textSrch').hide();
+    jQuery('#btnSrch').hide();
+
+    if (getUrlVars()["editable"] == 'true') {
+        areaSelect = jQuery('#pagediv0').children().imgAreaSelect({
+            handles : true,
+            instance : true,
+            onSelectEnd : function(img, selection) {
+                jQuery('#offsetX').val(Math.round(jQuery(img).offset().left));
+                jQuery('#imageX1').val(selection.x1);
+                jQuery('#imageY1').val(selection.y1);
+                jQuery('#imageX2').val(selection.x2);
+                jQuery('#imageY2').val(selection.y2);
+                jQuery('#imageWidth').val(selection.width);
+                jQuery('#imageHeight').val(selection.height);
+            }
+        });
+    } else {
+        jQuery('#pagediv0').children().click(function() {
+            jQuery("[selected=selected]").attr("selected", "").css("background-color", "rgb(127,127,0)").css("border", "3px solid yellow");
+            parent.window.clearObjectUrl();
+            parent.window.clearSelectedText();
+        });
+    }
 });
 
-function resetImage(x1,y1,x2,y2) {
-  areaSelect.setOptions({ show: true });
-  areaSelect.setSelection(x1,y1,x2,y2);
-  areaSelect.update();
+function resetImage(x1, y1, x2, y2) {
+    areaSelect.setOptions({
+        show : true
+    });
+    areaSelect.setSelection(x1, y1, x2, y2);
+    areaSelect.update();
 }
 
 function selectionVisible() {
-  return !(areaSelect.getSelection().width == 0 && areaSelect.getSelection().height == 0);
+    return !(areaSelect.getSelection().width == 0 && areaSelect.getSelection().height == 0);
 }
 
 function focusImageSelection(img, sync) {
-  if (img.getAttribute('selected') != 'selected') {
-    jQuery("[selected=selected]").attr("selected","")
-      .css("background-color","rgb(127,127,0)")
-      .css("border","3px solid yellow");
-    img.style.border = '3px solid purple';
-    img.style.backgroundColor = 'rgb(127,0,127)';
-    img.setAttribute('selected','selected');
+    if (img.getAttribute('selected') != 'selected') {
+        jQuery("[selected=selected]").attr("selected", "").css("background-color", "rgb(127,127,0)").css("border", "3px solid yellow");
+        img.style.border = '3px solid purple';
+        img.style.backgroundColor = 'rgb(127,0,127)';
+        img.setAttribute('selected', 'selected');
 
-    if (sync == true) {
-      parent.window.setObjectUrl(img.getAttribute("objectUrl"));
-      parent.window.setSelectedText(img.getAttribute("objectUrl"));
+        if (sync == true) {
+            parent.window.setObjectUrl(img.getAttribute("objectUrl"));
+            parent.window.setSelectedText(img.getAttribute("objectUrl"));
+        }
     }
-  }
 }
 
 function setSelectedImage(objectUrl) {
-  jQuery("[selected=selected]").attr("selected","")
-    .css("background-color","rgb(127,127,0)")
-    .css("border","3px solid yellow");
-  
-  var img = jQuery("[objectUrl='" + objectUrl + "']")[0];
-  img.style.border = '3px solid purple';
-  img.style.backgroundColor = 'rgb(127,0,127)';
-  img.setAttribute('selected','selected');
+    jQuery("[selected=selected]").attr("selected", "").css("background-color", "rgb(127,127,0)").css("border", "3px solid yellow");
+
+    var img = jQuery("[objectUrl='" + objectUrl + "']")[0];
+    img.style.border = '3px solid purple';
+    img.style.backgroundColor = 'rgb(127,0,127)';
+    img.setAttribute('selected', 'selected');
 }
 
 function clearSelectedImage() {
-  jQuery("[selected=selected]").attr("selected","")
-    .css("background-color","rgb(127,127,0)")
-    .css("border","3px solid yellow");
+    jQuery("[selected=selected]").attr("selected", "").css("background-color", "rgb(127,127,0)").css("border", "3px solid yellow");
 }
 
 function getSelectedObjectUrl() {
-  var selectedDivs = jQuery("[selected=selected]");
-  if (selectedDivs.length != 1) {
-    return;
-  }
-  return selectedDivs[0].getAttribute('objectUrl');
+    var selectedDivs = jQuery("[selected=selected]");
+    if (selectedDivs.length != 1) {
+        return;
+    }
+    return selectedDivs[0].getAttribute('objectUrl');
 }
--- www/textReader.js
+++ www/textReader.js
@@ -1,159 +1,151 @@
 function getUrlVars() {
-  var vars = {};
-  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-      vars[key] = value;
-  });
-  return vars;
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
 }
 
 window.onload = function() {
-  var url;
-  if(getUrlVars()["url"]) {
-      url = decodeURIComponent(getUrlVars()["url"]);
-  } else {
-      url = '/alignment/textinput.html';
-  }
+    var url;
+    if (getUrlVars()["url"]) {
+        url = decodeURIComponent(getUrlVars()["url"]);
+    } else {
+        url = '/alignment/textinput.html';
+    }
 
-  jQuery('#injected-text').load(url, function() {
-      setTimeout(iResize, 50);
-  });
-
-  jQuery("#container-div-2").scroll(function() { 
-      jQuery("#container-div-1").scrollTop(jQuery("#container-div-2").scrollTop());
-  });
- 
-  if (getUrlVars()["editable"] == 'true') {
-    jQuery("#container-div-1").click(function() {
-      jQuery("[selected=selected]").attr("selected","")
-          .attr("src","/alignment/link_black.png");
-    });
- 
-    jQuery("#container-div-2").click(function() {
-      jQuery("[selected=selected]").attr("selected","")
-          .attr("src","/alignment/link_black.png");
-    });
-  } else {
-    jQuery("#container-div-1").click(function() {
-      jQuery("[selected=selected]").attr("selected","")
-          .attr("src","/alignment/link_black.png");
-
-      parent.window.clearObjectUrl();
-      parent.window.clearSelectedImage();
+    jQuery('#injected-text').load(url, function() {
+        setTimeout(iResize, 50);
     });
 
-    jQuery("#container-div-2").click(function() {
-      jQuery("[selected=selected]").attr("selected","")
-          .attr("src","/alignment/link_black.png");
-
-      parent.window.clearObjectUrl();
-      parent.window.clearSelectedImage();
+    jQuery("#container-div-2").scroll(function() {
+        jQuery("#container-div-1").scrollTop(jQuery("#container-div-2").scrollTop());
     });
-  }
+
+    if (getUrlVars()["editable"] == 'true') {
+        jQuery("#container-div-1").click(function() {
+            jQuery("[selected=selected]").attr("selected", "").attr("src", "/alignment/link_black.png");
+        });
+
+        jQuery("#container-div-2").click(function() {
+            jQuery("[selected=selected]").attr("selected", "").attr("src", "/alignment/link_black.png");
+        });
+    } else {
+        jQuery("#container-div-1").click(function() {
+            jQuery("[selected=selected]").attr("selected", "").attr("src", "/alignment/link_black.png");
+
+            parent.window.clearObjectUrl();
+            parent.window.clearSelectedImage();
+        });
+
+        jQuery("#container-div-2").click(function() {
+            jQuery("[selected=selected]").attr("selected", "").attr("src", "/alignment/link_black.png");
+
+            parent.window.clearObjectUrl();
+            parent.window.clearSelectedImage();
+        });
+    }
 }
-
 function iResize() {
-  var sizeInt = parseInt(document.getElementById('injected-text').offsetHeight, 10) + 50;
+    var sizeInt = parseInt(document.getElementById('injected-text').offsetHeight, 10) + 50;
 
-  document.getElementById('annotations-div').height = sizeInt + 'px';
-  document.getElementById('annotations-div').style.height = sizeInt + 'px';
+    document.getElementById('annotations-div').height = sizeInt + 'px';
+    document.getElementById('annotations-div').style.height = sizeInt + 'px';
 }
 
 function focusText(img) {
-  var startOffset = img.getAttribute('startOffset');
-  var startOffsetXpath = img.getAttribute('startOffsetXpath');
-  var endOffset = img.getAttribute('endOffset');
-  var endOffsetXpath = img.getAttribute('endOffsetXpath');
-  
-  focusTextOffsetsWithXPaths(startOffset, startOffsetXpath, endOffset, endOffsetXpath);
+    var startOffset = img.getAttribute('startOffset');
+    var startOffsetXpath = img.getAttribute('startOffsetXpath');
+    var endOffset = img.getAttribute('endOffset');
+    var endOffsetXpath = img.getAttribute('endOffsetXpath');
+
+    focusTextOffsetsWithXPaths(startOffset, startOffsetXpath, endOffset, endOffsetXpath);
 }
 
 function focusTextOffsetsWithXPaths(startOffset, startOffsetXpath, endOffset, endOffsetXpath) {
-  var injectedText = document.getElementById('injected-text');
+    var injectedText = document.getElementById('injected-text');
 
-  if (window.getSelection && document.createRange) {
-    var sel = window.getSelection();
-    var range = document.createRange();
-    range.selectNodeContents(injectedText);
-    range.setStart(lookupElementByXPath(startOffsetXpath), startOffset);
-    range.setEnd(lookupElementByXPath(endOffsetXpath), endOffset);
-    sel.removeAllRanges();
-    sel.addRange(range);
-  } else if (document.selection && document.body.createTextRange) {
-    var textRange = document.body.createTextRange();
-    textRange.moveToElementText(injectedText);
-    textRange.setStart(lookupElementByXPath(startOffsetXpath), startOffset);
-    textRange.setEnd(lookupElementByXPath(endOffsetXpath), endOffset);
-    textRange.select();
-  }
+    if (window.getSelection && document.createRange) {
+        var sel = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(injectedText);
+        range.setStart(lookupElementByXPath(startOffsetXpath), startOffset);
+        range.setEnd(lookupElementByXPath(endOffsetXpath), endOffset);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (document.selection && document.body.createTextRange) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(injectedText);
+        textRange.setStart(lookupElementByXPath(startOffsetXpath), startOffset);
+        textRange.setEnd(lookupElementByXPath(endOffsetXpath), endOffset);
+        textRange.select();
+    }
 }
 
-function lookupElementByXPath(path) { 
-  var evaluator = new XPathEvaluator(); 
-  var result = evaluator.evaluate(path, document.documentElement, null,XPathResult.FIRST_ORDERED_NODE_TYPE, null); 
-  return  result.singleNodeValue; 
+function lookupElementByXPath(path) {
+    var evaluator = new XPathEvaluator();
+    var result = evaluator.evaluate(path, document.documentElement, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+    return result.singleNodeValue;
 }
 
 function focusTextOffsets(startOffset, endOffset) {
-  var injectedText = document.getElementById('injected-text');
+    var injectedText = document.getElementById('injected-text');
 
-  if (window.getSelection && document.createRange) {
-    var sel = window.getSelection();
-    var range = document.createRange();
-    range.selectNodeContents(injectedText);
-    range.setStart(injectedText.childNodes[0], startOffset);
-    range.setEnd(injectedText.childNodes[0], endOffset);
-    sel.removeAllRanges();
-    sel.addRange(range);
-  } else if (document.selection && document.body.createTextRange) {
-    var textRange = document.body.createTextRange();
-    textRange.moveToElementText(injectedText);
-    textRange.setStart(injectedText.childNodes[0], startOffset);
-    textRange.setEnd(injectedText.childNodes[0], endOffset);
-    textRange.select();
-  }
+    if (window.getSelection && document.createRange) {
+        var sel = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(injectedText);
+        range.setStart(injectedText.childNodes[0], startOffset);
+        range.setEnd(injectedText.childNodes[0], endOffset);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (document.selection && document.body.createTextRange) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(injectedText);
+        textRange.setStart(injectedText.childNodes[0], startOffset);
+        textRange.setEnd(injectedText.childNodes[0], endOffset);
+        textRange.select();
+    }
 }
 
 function highlightImage(img, sync) {
-  if (img.getAttribute('selected') != 'selected') {
-    jQuery("[selected=selected]").attr("selected","")
-      .attr("src","/alignment/link_black.png");
-    img.src = '/alignment/link_yellow.png';
-    img.setAttribute('selected','selected');
+    if (img.getAttribute('selected') != 'selected') {
+        jQuery("[selected=selected]").attr("selected", "").attr("src", "/alignment/link_black.png");
+        img.src = '/alignment/link_yellow.png';
+        img.setAttribute('selected', 'selected');
 
-    focusText(img);
+        focusText(img);
 
-    if (getUrlVars()["editable"] != 'true') {
-      parent.window.setObjectUrl(img.getAttribute("objectUrl"));
-      parent.window.setSelectedImage(img.getAttribute("objectUrl"));
+        if (getUrlVars()["editable"] != 'true') {
+            parent.window.setObjectUrl(img.getAttribute("objectUrl"));
+            parent.window.setSelectedImage(img.getAttribute("objectUrl"));
+        }
     }
-  }
 }
 
 function setSelectedText(objectUrl) {
-  jQuery("[selected=selected]").attr("selected","")
-      .attr("src","/alignment/link_black.png");
-  var img = jQuery("[objectUrl=" + objectUrl + "]")[0];
-  img.setAttribute("selected","selected");
-  img.setAttribute("src","/alignment/link_yellow.png");
+    jQuery("[selected=selected]").attr("selected", "").attr("src", "/alignment/link_black.png");
+    var img = jQuery("[objectUrl=" + objectUrl + "]")[0];
+    img.setAttribute("selected", "selected");
+    img.setAttribute("src", "/alignment/link_yellow.png");
 
-  jQuery("#container-div-1").scrollTop(img.offsetTop - (jQuery("#container-div-1")[0].offsetHeight / 4));
-  jQuery("#container-div-2").scrollTop(jQuery("#container-div-1").scrollTop());
-  jQuery("#container-div-1").scrollTop(jQuery("#container-div-2").scrollTop());
+    jQuery("#container-div-1").scrollTop(img.offsetTop - (jQuery("#container-div-1")[0].offsetHeight / 4));
+    jQuery("#container-div-2").scrollTop(jQuery("#container-div-1").scrollTop());
+    jQuery("#container-div-1").scrollTop(jQuery("#container-div-2").scrollTop());
 
-  focusText(img);
+    focusText(img);
 }
 
 function clearSelectedText() {
-  jQuery("[selected=selected]").attr("selected","")
-    .attr("src","/alignment/link_black.png");
+    jQuery("[selected=selected]").attr("selected", "").attr("src", "/alignment/link_black.png");
 
-  if (window.getSelection) {
-    if (window.getSelection().empty) {
-      window.getSelection().empty();
-    } else if (window.getSelection().removeAllRanges) {
-      window.getSelection().removeAllRanges();
+    if (window.getSelection) {
+        if (window.getSelection().empty) {
+            window.getSelection().empty();
+        } else if (window.getSelection().removeAllRanges) {
+            window.getSelection().removeAllRanges();
+        }
+    } else if (document.selection) {
+        document.selection.empty();
     }
-  } else if (document.selection) {
-    document.selection.empty();
-  }
 }
